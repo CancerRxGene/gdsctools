44b82dc80133237eaeabe2c5e4288897acc75ce1
cokelaer <cokelaer@gmail.com>
2015-10-14 09:49:31 +0100

    improve plotting

--- gdsctools/boxswarm.py
+++ gdsctools/boxswarm.py
@@ -1,7 +1,5 @@
-import pandas as pd
-from easydev import Progress, AttrDict
 import pylab
-
+import numpy as np
 
 
 
@@ -58,12 +56,23 @@ class BoxSwarm(object):
         self.colors = ['lightgrey', 'blue']
         self.hold = False
         self.title = ''
+        self.lw = 2
 
-    def beeswarm(self, data, ratio=2.):
+    def beeswarm(self, data, position, ratio=2.):
+        """
+
+        dat = data + (U()-0.5)/2 * factor
+
+        factor = 1 or distance to media 1 / (abs(median-x)/std)
+        """
         N = len(data)
-        newdata = data + (pylab.random(N) - 0.5)/float(ratio)
-        return newdata
+        m = np.median(data)
+        sd = np.std(data)
+        # arctan function to have a tapering window
+        factor = 1. - np.abs(np.arctan((data-m)/sd)/1.570796)  # pi/2
 
+        newdata = position + (pylab.random(N) - 0.5)/float(ratio) * factor
+        return newdata
 
     def plot(self, vert=True, widths=0.5, **kwargs):
         """
@@ -81,73 +90,77 @@ class BoxSwarm(object):
 
             color = self.colors[i%len(self.colors)]
             if vert is True:
-                X, Y = self.beeswarm([i+1] * N), vector
+                X, Y = self.beeswarm(vector, i+1), vector
             else:
-                X, Y = vector, self.beeswarm([i+1] * N)
+                X, Y = vector, self.beeswarm(vector, i+1)
             pylab.plot(X, Y,
                 'o', markersize=15, markerfacecolor=color,
                 markeredgewidth=0, alpha=0.5)
 
-        means = [pylab.mean(data) for data in ordered_data]
-        stds = [pylab.std(data) for data in ordered_data]
 
+        #show means but not outliers
         d = pylab.boxplot(ordered_data, widths=self.widths,
-                vert=vert, patch_artist=True)
+               vert=vert, patch_artist=True, 
+                positions=range(1, len(ordered_data)+1),
+            showmeans=True, showfliers=False)
 
         # for further tuning if needed.
         self.tuning = d
+        # This is now in matplotlib 1.4.3 (dots instead of lines
+        # though)
+        """
+        means = [pylab.mean(data) for data in ordered_data]
+        stds = [pylab.std(data) for data in ordered_data]
         for i, this in enumerate(means):
             if vert is True:
                 x1 = (i+1) - widths/2. / 1.5
                 x2 = (i+1) + widths/2. / 1.5
-                X = pylab.array([x1,x2])
+                X = pylab.array([x1, x2])
                 pylab.plot(X, [this, this], lw=2, color='r')
             else:
                 y1 = (i+1) - widths/2. / 1.5
                 y2 = (i+1) + widths/2. / 1.5
-                Y = pylab.array([y1,y2])
+                Y = pylab.array([y1, y2])
                 pylab.plot([this, this], Y, lw=2, color='r')
-
+        """
         for i, this in enumerate(d['boxes']):
             this.set_color('k')
-            this.set_linewidth(2)
+            this.set_linewidth(self.lw)
             color = self.colors[i%len(self.colors)]
             this.set_facecolor(color)
             this.set_alpha(0.4) # 0.4 is less than the alpha of the dots to ...
             # ... so as to see the dots inside the boxes
             this.set_zorder(10) # this moves the box on top of all dots
         for this in d['caps']:
-            this.set_linewidth(2)
+            this.set_linewidth(self.lw)
         for this in d['whiskers']:
-            this.set_linewidth(2)
+            this.set_linewidth(self.lw)
         for this in d['medians']:
-            this.set_linewidth(2)
-        for this in d['fliers']:
-            #delete ?
-            this.set_linewidth(0)
-
-        N = len(self.names)
+            this.set_linewidth(self.lw)
+       
+        # we will extend the limits by 5%
+        m = min([min(this) for this in self.data.values()])
+        M = max([max(this) for this in self.data.values()])
+        extend = 0.05
+        R = (M-m) * extend
+        X, Y = range(1, len(self.names)+1), self.names
+        Y = [y.replace("_", "\_") for y in Y]
         if vert is True:
             pylab.ylabel(self.ylabel, fontsize=self.fontsize)
-            pylab.xticks(range(1, N+1), self.names, fontsize=self.fontsize,
-                rotation=90)
+            pylab.xticks(X, Y, fontsize=self.fontsize, rotation=90)
             pylab.ylabel(self.xlabel, fontsize=self.fontsize)
             pylab.yticks(pylab.yticks()[0], fontsize=self.fontsize)
-            m = min([min(this) for this in self.data.values()])
-            M = max([max(this) for this in self.data.values()])
-            R = (M-m)* 0.05
             pylab.ylim([m-R,M+R])
         else:
             pylab.xlabel(self.xlabel, fontsize=self.fontsize)
-            pylab.yticks(range(1,N+1), self.names, fontsize=self.fontsize,
-                rotation=00)
+            pylab.yticks(X, Y, fontsize=self.fontsize, rotation=00)
             pylab.ylabel(self.ylabel, fontsize=self.fontsize)
             pylab.xticks(pylab.xticks()[0], fontsize=self.fontsize)
-            m = min([min(this) for this in self.data.values()])
-            M = max([max(this) for this in self.data.values()])
-            R = (M-m) * 0.05
             pylab.xlim([m-R,M+R])
+
         pylab.title(self.title, fontsize=self.fontsize)
         pylab.grid()
         pylab.tight_layout()
 
+        # TODO: is this returning the correct axes ?
+        return pylab.gca()
