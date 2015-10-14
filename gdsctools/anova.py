dcb20192e77777baa362511af6506ea3849ddbe2
cokelaer <cokelaer@gmail.com>
2015-10-14 21:47:19 +0100

    fix import for python3

--- gdsctools/anova.py
+++ gdsctools/anova.py
@@ -15,7 +15,7 @@ try:
     from cno.misc.profiler import do_profile
 except:
     pass
-import cohens, glass
+from gdsctools import cohens, glass
 # See reader module to get the format. The file IC50_input.txt was provided
 # by Howard as a test case
 #data = reader.IC50()
