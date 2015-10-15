c5bdef466b573ab79ad55bc9b6bd8fc26e3a6a92
cokelaer <cokelaer@gmail.com>
2015-10-15 22:32:34 +0100

    improve the readers [ci skip]

--- gdsctools/reader.py
+++ gdsctools/reader.py
@@ -5,11 +5,21 @@
 """
 import pandas as pd
 import pylab
-
+import easydev
 
 class Reader(object):
-    def __init__(self, filename):
-        self.filename = filename
+    """
+
+    
+    """
+    def __init__(self, filename, sep="\t"):
+        """
+
+        :param input: could be a filename
+
+        """
+        self._filename = filename
+        self._sep = sep
 
     def read_matrix_from_r(self, name):
         print("Reading matrix %s " % (name))
@@ -25,43 +35,78 @@ class Reader(object):
         df.index = [x.strip() for x in rnames]
         return df
 
-    def save(self, tag='test'):
-        # There are 14 digits in the original file, so let us write
-        # with 14 digits as well.
-        self.ic50.to_csv("test_ic50.tsv", sep="\t", float_format="%.14f")
-        self.features.to_csv("test_features.tsv", sep="\t",
-                float_format="%.14f")
+    def __str__(self):
+        self.df.info()
+        return ""
 
 
-class IC50(Reader):
-    """Reader of ANOVA data set created by Francesco
+class CosmicRows(object):
+    """Parent class to IC50 and GenomicFeatures"""
+    def _get_cosmic(self):
+        return list(self.df.index)
+    cosmicIds = property(_get_cosmic, doc="return list of cosmic ids")
 
-    This data seems to have all genomic feature and drug IC50 in the same matrix
-    and each row correspond to a cellline.
 
+class IC50(Reader, CosmicRows):
+    """Reader of IC50 data set
+
+    Matrix containing the IC50 for each drug. First column must
+    contain the COSMIC ID and other columns are the DRUGS
+
+    DRUG label must be "Drug_XX_IC50"
+
+    Extra columns will be ignored.
 
     """
-    def __init__(self, filename='ANOVA_input.txt'):
-        super(IC50, self).__init__(filename)
+    def __init__(self, filename='ANOVA_input.txt', sep="\t"):
+        super(IC50, self).__init__(filename, sep=sep)
 
-        self._read_data()
+        if isinstance(filename, str):
+            self.rawdf = pd.read_csv(self._filename, sep=self._sep)
+            columns = ['COSMIC ID']
+            columns += [x for x in self.rawdf.columns if x.startswith('Drug')]
+            self.df = self.rawdf[columns].copy() # is copy  required ?
+            self.df.set_index('COSMIC ID', inplace=True)
+        else:
+            self.df = filename.df.copy()
 
-    def _read_data(self):
-        self.rawdf = pd.read_csv(self.filename, sep='\t')
-
-        columns = ['COSMIC ID'] +[x for x in self.rawdf.columns if x.startswith('Drug')]
-        self.ic50 = self.rawdf[columns].copy() # is copy  required ?
-        self.ic50.set_index('COSMIC ID', inplace=True)
-
-        columns = [x for x in self.rawdf.columns if
-                x.startswith('Drug') is False]
-        self.features = self.rawdf[columns]
-        self.features.set_index('COSMIC ID', inplace=True)
+    def _get_drugs(self):
+        return list(self.df.columns)
+    drugIds = property(_get_drugs)
 
 
-class Feature(Reader):
-    def __init__(self, filename):
-        pass
+class GenomicFeatures(Reader, CosmicRows):
+    def __init__(self, filename=None, sep="\t"):
+        # first reset the filename to the shared data (if not provided)
+        if filename is None:
+            filename = easydev.get_share_file('gdsctools', 'data', 
+                            'genomic_features.tsv')
+        super(GenomicFeatures, self).__init__(filename)
+
+        if isinstance(filename, str):
+            self.df = pd.read_csv(self._filename, sep=self._sep)
+            assert 'COSMIC ID' in self.df.columns, \
+                "the features input file must contains a column named COSMIC ID"
+            self.df.set_index('COSMIC ID', inplace=True)
+        else:
+            self.df = filename.df.copy()
+
+        # There are several types of features e.g., mutation, CNA,
+        # methylation but all are stored within the same file
+        # Besides, the 3 first columns are
+        #
+        # Sample Name
+        # Tissue Factor Value
+        # MS-instability Factor Value
+        #
+        # Note the spaces
+        # columsn are tab-delimited
+
+    def _get_features(self):
+        return list(self.df.columns)
+    features = property(_get_features)
+
+
 
 
 class PANCAN(Reader):
@@ -70,7 +115,7 @@ class PANCAN(Reader):
         # Remove R dependencies
         from biokit.rtools import RSession
         self.session = RSession()
-        self.session.run('load("%s")' %self.filename)
+        self.session.run('load("%s")' %self._filename)
         self.df = self.read_matrix_from_r('MoBEM')
 
 
@@ -80,7 +125,7 @@ class Extra(Reader):
         # Remove R dependencies
         from biokit.rtools import RSession
         self.session = RSession()
-        self.session.run('load("%s")' %self.filename)
+        self.session.run('load("%s")' %self._filename)
 
         # 3 identical matrices containing AUC, IC50 and
         self.dfAUCv17= self.read_matrix_from_r('dfAUCv17')
@@ -102,7 +147,7 @@ class Extra(Reader):
         pylab.hist(data, bins=bins, normed=True)
         pylab.grid(True)
         pylab.xlabel('Residuals')
-        pylab.ylabel('#')
+        pylab.ylabel(r'\#')
 
     def scatter(self):
         from biokit import scatter
@@ -117,7 +162,7 @@ class Extra(Reader):
         pylab.hist(data, bins=bins, normed=True)
         pylab.grid(True)
         pylab.xlabel('IC50')
-        pylab.ylabel('#')
+        pylab.ylabel(r'\#')
 
     def hist_auc(self, bins=100):
         data = [x for x in self.dfAUCv17.fillna(0).values.flatten() if x != 0]
@@ -125,6 +170,6 @@ class Extra(Reader):
         pylab.hist(data, bins=bins, normed=True)
         pylab.grid(True)
         pylab.xlabel('AUC')
-        pylab.ylabel('#')
+        pylab.ylabel(r'\#')
 
 
