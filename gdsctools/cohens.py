d6667d96d6e81e92f3b1de5a6b36ac9e395261c9
cokelaer <cokelaer@gmail.com>
2015-10-19 18:05:38 +0100

    decompose cohens code a little bit

--- gdsctools/cohens.py
+++ gdsctools/cohens.py
@@ -13,15 +13,17 @@ def cohens(x, y):
     x = np.array(x)
     y = np.array(y)
 
-    Nx = len(x) - 1
-    Ny = len(y) - 1
+    Nx = len(x) - 1.  # note the dot to cast to float
+    Ny = len(y) - 1.
     # mean difference: 
     md = np.abs(x.mean() - y.mean()) 
 
     # here, we want same as in R that is unbiased variance
     # so we use ddof = 1
-    csd = Nx * x.var(ddof=1) + Ny * y.var(ddof=1)
-    csd /= float(Nx + Ny)  # make sure this is float
+    xv = x.var(ddof=1)
+    yv = y.var(ddof=1)
+    csd = Nx * xv + Ny * yv
+    csd /= Nx + Ny  # make sure this is float
     csd = np.sqrt(csd)
 
     return md / csd
