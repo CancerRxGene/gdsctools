0e6bb77ddcbdff8424ad9f954e4de2dc2545237c
cokelaer <cokelaer@gmail.com>
2015-10-19 18:05:07 +0100

    fix version

--- gdsctools/__init__.py
+++ gdsctools/__init__.py
@@ -1,6 +1,6 @@
 import pkg_resources
 try:
-    version = pkg_resources.require("dreamtools")[0].version
+    version = pkg_resources.require("gdsctools")[0].version
     __version__ = version
 except:
     # update this manually is possible when the version in the
