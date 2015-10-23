5cb26cc29728f6f20f60b07db1c6e58d36fc6fa3
cokelaer <cokelaer@gmail.com>
2015-10-23 14:31:40 +0100

    add missing file for testing.

---
+++ test/gdsctools/tools.py
@@ -0,0 +1,13 @@
+from gdsctools.reader import IC50
+
+
+
+def get_data(filename='IC50_10drugs.tsv'):
+    import os
+    try:
+        r = IC50(filename)
+    except:
+        r = IC50('test/gdsctools' + os.sep +  filename)
+    return r
+
+
