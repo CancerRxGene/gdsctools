75b95ce1288a1d6d00465e36e234edc030325939
Thomas Cokelaer <tc14@loan105307.internal.sanger.ac.uk>
2015-10-30 14:47:58 +0000

    add missing dependency

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     # jinja2 and mpl3d are for the volcano plots.
     install_requires = ['numpy', 'matplotlib>=1.4.3', 
         'pandas>=0.16.2', 'easydev>=0.9.2', 'scipy', "colormap>=0.9.6",
-        "beautifulsoup4", 'mpld3', 'jinja2'],
+        "beautifulsoup4", 'mpld3', 'jinja2', 'browse'],
 
     entry_points = {
         'console_scripts': [
