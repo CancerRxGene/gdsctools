6ab36b39da358d4a9a72345f9f3cb5e232ef6da0
cokelaer <cokelaer@gmail.com>
2015-11-03 11:30:27 +0000

    add new test

---
+++ test/gdsctools/test_boxplots.py
@@ -0,0 +1,23 @@
+from gdsctools.anova import ANOVA
+from easydev import assert_list_almost_equal
+try:
+    from . import tools
+except:
+    import tools
+from gdsctools.boxplots import BoxPlots
+
+
+def test_get_boxplot_data():
+    an = ANOVA(tools.get_data())
+    odof = an._get_one_drug_one_feature_data('Drug_1047_IC50','TP53_mut')
+
+    bb = BoxPlots(odof)
+
+    data = bb._get_boxplot_data(mode='msi')
+    assert data[1] == ['***MSI-stable neg', '***MSI-stable pos',
+                  '**MSI-unstable neg',  '**MSI-unstable pos']
+    expected = [2.0108071495663922e-47, 0.0012564798887037905]
+    assert_list_almost_equal([data[2][0], data[2][1]], expected)  
+
+
+
