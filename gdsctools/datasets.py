a13663f328601528d3f677a6eac34be8b81d1987
cokelaer <cokelaer@gmail.com>
2015-11-09 22:15:25 +0000

    add new dataset.

--- gdsctools/datasets.py
+++ gdsctools/datasets.py
@@ -14,25 +14,48 @@
 #  website: http://github.com/CancerRxGene/gdsctools
 #
 ##############################################################################
-"""Data sets provided with GDSCTools"""
+"""Data sets provided with GDSCTools
+
+The datasets may be for testing purpose:
+
+- :attr:`ic50_test`
+- :attr:`drug_test`
+- :attr:`cosmic_builder_test`
+
+or informative:
+
+- :attr:`cancer_cell_lines`
+
+or used in analysis:
+
+- :attr:`genomic_features`
+"""
 import easydev
 
 
-__all__ = ['dataset', 'ic50_test', 'genomic_features', 'drug_test']
+__all__ = ['Data', 'dataset', 'ic50_test', 'genomic_features', 'drug_test',
+           "cosmic_builder_test", "cancer_cell_lines"]
 
 
 class Data(object):
-    """A class to hold information about a dataset
+    """A convenience class to hold information about a dataset
 
 
-    Can be used as input to :class:`ANOVA` instance.
+    Each :class:`Data` instance contains information about :
+
+    #. the file location (:attr:`filename`)
+    #. the data description (:attr:`description`)
+    #. the authors (:attr:`authors`)
+
+    But the data is not stored and users must read the data set using
+    their own tools. 
     """
     def __init__(self):
-        #: where is located the data set
-        self.filename = None
-        #: a short description
+        #: where is located the data set (full path)
+        self.filename = None 
+        #: a short description (string)
         self.description = "No description"
-        #: list of authors
+        #: list of authors (string)
         self.authors = 'GDSC consortium'
 
     def __str__(self):
@@ -40,10 +63,6 @@ class Data(object):
         txt += 'description: %s\n' % self.description
         txt += 'authors: %s\n' % self.authors
         return txt
-
-
-registered = ['ic50_test', 'genomic_features', 'drug_test',
-    'cancer_cell_line']
 
 
 def dataset(dataname):
@@ -61,6 +80,8 @@ def dataset(dataname):
         ic50_test.filename
 
     """
+    registered = ['ic50_test', 'genomic_features', 'drug_test',
+        'cancer_cell_lines', 'cosmic_builder_test']
     easydev.check_param_in_list(dataname, registered)
 
     d = Data()
@@ -81,17 +102,30 @@ def dataset(dataname):
                 'data', 'cancer_cell_lines.csv')
         d.description = "List of cosmic identifiers with the corresponding "+\
             "name, tissue and sub tissue types"
+    elif dataname == 'cosmic_builder_test':
+        d.filename = easydev.get_share_file('gdsctools',
+                'data', 'cosmic_builder_test.txt')
+        d.description = "An example of flat file to be read by COSMICFetcher"
+
     return d
 
-#: dataset with IC50s for 10 drugs (for testing)
+# ALIASES
+
+#: Dataset with IC50s for 10 drugs (for testing)
 ic50_test = dataset('ic50_test')
 
-#: dataset with genomic features for 1001 cell lines and 680 features
+#: Dataset with genomic features for 1001 cell lines and 680 features
 genomic_features = dataset('genomic_features')
 
-#: dataset with drug name and targets (for testing)
+#: Dataset with drug name and targets (for testing)
 drug_test = dataset('drug_test')
 
+#: Dataset with cancer cell lines name / cosmic id/ tissue type and sub type
+cancer_cell_lines = dataset('cancer_cell_lines')
 
-#: cancer cell lines table with name / cosmic id/ tissue type and sub type
-cancer_cell_lines = dataset('cancer_cell_line')
+#: Example of flat file to be read by COSMICFetcher
+cosmic_builder_test = dataset("cosmic_builder_test")
+
+
+
+
