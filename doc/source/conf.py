727d5978427bfacb54449b78e9277a3746b271ec
cokelaer <cokelaer@gmail.com>
2015-11-10 16:00:28 +0000

    fix sphinx theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -136,15 +136,9 @@ modindex_common_prefix = ["gdsctools."]
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 html_theme = 'standard'
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:  # only import and set the theme if we're building docs locally
-     import sphinx_rtd_theme
-     html_theme = 'sphinx_rtd_theme'
-     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# otherwise, readthedocs.org uses their theme by default, so no
-# need to specify it
-
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -153,7 +147,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 #html_theme_options = {'homepage': init_sphinx.url}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [get_path_sphinx_themes()]
+#html_theme_path = [get_path_sphinx_themes()]
 
 
 
