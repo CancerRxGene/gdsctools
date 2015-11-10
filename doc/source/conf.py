713f1ac2791aad895eb6b43f1b460e97d227f7aa
cokelaer <cokelaer@gmail.com>
2015-11-10 15:52:44 +0000

    choose rtd theme and fix a typo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -150,14 +150,11 @@ modindex_common_prefix = ["gdsctools."]
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 html_theme = 'standard'
-# on_rtd is whether we are on readthedocs.org
-#import os
-#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-#if not on_rtd:  # only import and set the theme if we're building docs locally
-#    import sphinx_rtd_theme
-#html_theme = 'sphinx_rtd_theme'
-#    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+     import sphinx_rtd_theme
+     html_theme = 'sphinx_rtd_theme'
+     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # otherwise, readthedocs.org uses their theme by default, so no
 # need to specify it
