6ff626e8e5f5f1a130e1e30b92019a8767c69615
cokelaer <cokelaer@gmail.com>
2015-11-10 16:07:11 +0000

    update dependencies

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ if on_rtd is True:  # only import and set the theme if we're building docs
 else:
     install_requires = ['numpy', 'matplotlib>=1.4.3',
         'pandas>=0.16.2', 'easydev>=0.9.3', 'scipy', "colormap>=0.9.6",
-        "beautifulsoup4", 'mpld3', 'jinja2'],
+        "beautifulsoup4", 'mpld3', 'jinja2', 'statsmodels'],
 
 
 
