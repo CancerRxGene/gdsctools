170ea728b903f1f5c3ae9e4ee2f8eb003017a3c4
cokelaer <cokelaer@gmail.com>
2015-11-10 14:33:02 +0000

    change theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -157,7 +157,19 @@ modindex_common_prefix = ["gdsctools."]
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+# html_theme = 'default'
+# on_rtd is whether we are on readthedocs.org
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no
+# need to specify it
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
