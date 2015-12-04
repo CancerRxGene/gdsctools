f3cf35fa7a63b43748b5797613818fee4c51051d
cokelaer <cokelaer@gmail.com>
2015-12-04 19:34:04 +0000

    add test for new class COSMICInfo

--- test/gdsctools/test_cosmictools.py
+++ test/gdsctools/test_cosmictools.py
@@ -1,5 +1,10 @@
-from gdsctools.cosmictools import COSMICFetcher, COSMIC
+from gdsctools.cosmictools import COSMICFetcher, COSMIC, COSMICInfo
 from gdsctools.datasets import cosmic_builder_test
+
+def test_cosmic_info():
+    c = COSMICInfo()
+    assert c.get(930301, 'SAMPLE_NAME') == 'VMRC-MELG'
+
 
 def test_cosmic_fetcher():
     c = COSMICFetcher(cosmic_builder_test.filename)
