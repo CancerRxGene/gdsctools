b43f36b494d1f2de71f170e22904135a86f74fa5
cokelaer <cokelaer@gmail.com>
2016-05-23 12:18:26 +0200

    add draf test for the future OmniBEM class

---
+++ test/gdsctools/test_omnibem.py
@@ -0,0 +1,13 @@
+from gdsctools.omnibem import OmniBEM
+
+"""
+test_ga = ".."
+test_gene_list = "..."
+test_expected_filtered_output = "..."
+
+def test_omnibem():
+    o = OmniBEM(test_ga)
+    results = o.filter(test_gene_list)
+    expected = CSV2DF(test_expected_filtered_output)
+    assert results == expected
+"""
