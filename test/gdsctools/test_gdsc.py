55a7c849f95675c70e93eb28324953777f173f67
cokelaer <cokelaer@gmail.com>
2016-05-25 22:53:32 +0200

    update test

--- test/gdsctools/test_gdsc.py
+++ test/gdsctools/test_gdsc.py
@@ -1,4 +1,4 @@
-from gdsctools import GDSC, gdsctools_data, ANOVA
+from gdsctools import gdsctools_data, ANOVA
 from gdsctools.gdsc import IC50Cluster
 
 
@@ -6,15 +6,6 @@ def test_IC50Cluster():
     dataset = gdsctools_data("test_v18_clustering.tsv")
     ic50 = IC50Cluster(dataset)
 
-    # there are still 6 duplicared drugs not clustered because they 
-    # have lots of valid data in each
-    assert len(ic50.info()) == 6
-    assert 1510 in ic50.info().DRUG_ID.values
-
-    assert "Drug_1032_0.5_IC50" in ic50.df.columns
-    assert "Drug_1032_2_IC50" in ic50.df.columns
-
-    ic50.cleanup()
 
     # In this data set, a drug is reported 3 times (1211) and should appear 
     # as follows:
