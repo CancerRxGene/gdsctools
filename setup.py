d533b792be336a05005d618d721a01447f4801ef
cokelaer <cokelaer@gmail.com>
2016-05-25 22:53:20 +0200

    remove mpld3

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ if on_rtd is True:  # only import and set the theme if we're building docs
 else:
     install_requires = ['numpy', 'matplotlib>=1.4.3',
         'pandas>=0.16.2', 'easydev>=0.9.18', 'scipy', "colormap>=0.9.7",
-        'mpld3', 'jinja2', 'statsmodels', "scikit-learn", "reports>=0.1.8"],
+        'jinja2', 'statsmodels', "scikit-learn", "reports>=0.1.8"],
 
 
 packages = find_packages()
