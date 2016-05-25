cc3b06a735196de5e864d5a739bb4687c015ee53
cokelaer <cokelaer@gmail.com>
2016-05-25 11:39:03 +0200

    add docstrings

--- gdsctools/omnibem.py
+++ gdsctools/omnibem.py
@@ -8,40 +8,62 @@ __all__ = ["OmniBEMBuilder"]
 
 
 class OmniBEMBuilder(object):
-    """Create a BEM (Genomic Feature) file
+    """Utility to create :class:`~gdsctools.readers.GenomicFeatures` instance from BEM data
+
+    Starting from an example provided in GDSCTools
+    (test_omnibem_genomic_alteration.csv.gz), here is the code to get a data
+    structure compatible with the :class:`GenomicFeature`, which can then be
+    used as input to the :class:`~gdsctools.anova.ANOVA` class.
 
     ::
 
-        bem = OmniBEM("data.csv")
-        # optional
+        from gdsctools import gdsctools_data, OmniBEM, GenomicFeatures
+
+        input_data = gdsctools_data("test_omnibem_genomic_alteration.csv.gz")
+        bem = OmniBEM(input_data)
+
+        # You may filter the data for instance to keep only a set of genes
         bem.filter_by_gene_list(gene_list)
+
+        # Finally, create a MoBEM dataframe
         mobem = bem.get_mobem()
 
-        # Sort genes by number of times they have an alteration
-        bem.unified.sort_values(by="IDENTIFIER")
-
-
-        # top genes represented multiple times i
-        bem.unified.sort_values(by="IDENTIFIER", ascending=False).head(20)
-
-
-        bem = OmniBEMBuilder(gdsctools_data("test_omnibem_genomic_alterations.csv.gz"))
+        # You may filter with other functions(e.g., to keep only Methylation
+        # features
         bem.filter_by_type_list(["Methylation"])
         mobem = bem.get_mobem()
+
+        # Then, let us create a dataframe that is compatible with
+        # GenomicFeature. We just need to make sure the columns are correct
         mobem.columns = [x.replace("TISSUE_TYPE", "TISSUE_FACTOR") for x in mobem.columns]
         mobem[[x for x in mobem.columns if x!="SAMPLE"]]
         gf = GenomicFeatures(mobem[[x for x in mobem.columns if x!="SAMPLE"]])
+
+
+        # Now, we can perform an ANOVA analysis:
         an = ANOVA(ic50_test, gf)
         results = an.anova_all()
         results.volcano()
 
-    You can filter by, gene, cosmic identifiers, sample name, type of
-    alterations.
+    .. note:: The underlying data is stored in the attribute :attr:`df`.
 
-
-    - merge features or not.
     """
     def __init__(self, genomic_alteration):
+        """.. rubric:: Constructor
+
+
+        The input must be a CSV file (gzipped or not) with the following
+        columns::
+
+            COSMIC_ID:
+            TISSUE_TYPE: e.g. Methylation,
+            SAMPLE: this should correspond to the COSMID_ID
+            GENE:
+            IDENTIFIER: required but may be removed (rows can be used as
+                identifier indeed
+
+
+        """
         self.df = Reader(genomic_alteration).df
         # Note that if we keep NA, the groupby will not work as expected.
         self.df.fillna("", inplace=True)
@@ -50,6 +72,12 @@ class OmniBEMBuilder(object):
         self._update_unified()
 
     def _update_unified(self):
+        """# Sort genes by number of times they have an alteration
+        bem.unified.sort_values(by="IDENTIFIER")
+
+        # top genes represented multiple times i
+        bem.unified.sort_values(by="IDENTIFIER", ascending=False).head(20)
+        """
         # In R this is an aggregate function. In Pandas a groupby + aggregate
         # http://pandas.pydata.org/pandas-docs/stable/comparison_with_r.html
         groups = self.df.groupby(by=["COSMIC_ID", "GENE", "SAMPLE",
@@ -66,6 +94,11 @@ class OmniBEMBuilder(object):
         self.summary = df
 
     def get_mobem(self):
+        """Return a dataframe compatible with ANOVA analysis
+
+
+
+        """
         # Select gene that appear at least a minimum number of times
         #agg = self.unified.groupby("GENE")["GENE"].count()
         #self.selection = agg[agg>=minimum_gene]
@@ -84,10 +117,12 @@ class OmniBEMBuilder(object):
         return df.count().sort_values(ascending=False).head(N)
 
     def filter_by_gene_list(self, genes, minimum_gene=3):
-        """
+        """Keeps only required genes
 
-        :param genes: a list of genes or a filename
-
+        :param genes: a list of genes or a filename (a CSV file with a column
+            named GENE).
+        :param minimum_gene: genes with not enough entries are remove
+            (defaults to 3)
 
         """
         if isinstance(genes, str):
@@ -107,18 +142,34 @@ class OmniBEMBuilder(object):
         self._update_unified()
 
     def filter_by_type_list(self, type_list):
+        """Keeps only required type"""
         self.df = self.df.query("TYPE in @type_list")
         self._update_unified()
 
     def filter_by_sample_list(self, sample_list):
+        """Filter data to keep sample in sample_list"""
         self.df = self.df.query("SAMPLE in @sample_list")
         self._update_unified()
 
     def filter_by_cosmic_list(self, cosmic_list):
+        """Filter data to keep cosmic ids in cosmic_list"""
         self.df = self.df.query("COSMIC_ID in @cosmic_list")
         self._update_unified()
 
     def plot_number_alteration_by_tissue(self):
+        """Plot number of alterations
+
+        .. plot::
+            :width: 50%
+            :include-source:
+
+            from gdsctools import *
+            data = gdsctools_data("test_omnibem_genomic_alterations.csv.gz")
+            bem = OmniBEMBuilder(data)
+            bem.filter_by_gene_list(gdsctools_data("test_omnibem_genes.txt"))
+            bem.plot_number_alteration_by_tissue()
+
+        """
         count = self.unified.groupby(['TISSUE_TYPE'])['GENE'].count()
         count.sort_values(inplace=True, ascending=False)
         count.plot(kind="bar")
@@ -129,9 +180,22 @@ class OmniBEMBuilder(object):
         except:pass
 
     def plot_alterations_per_cellline(self):
+        """Plot number of alterations
+
+        .. plot::
+            :width: 50%
+            :include-source:
+
+            from gdsctools import *
+            data = gdsctools_data("test_omnibem_genomic_alterations.csv.gz")
+            bem = OmniBEMBuilder(data)
+            bem.filter_by_gene_list(gdsctools_data("test_omnibem_genes.txt"))
+            bem.plot_alterations_per_cellline()
+        """
         df = self.summary.sort_values("fraction", ascending=False)
         df.plot(y="fraction", legend=False, kind='bar', fontsize=10, width=0.8)
         pylab.ylabel("Alterations per cell line")
+        pylab.grid()
         try:pylab.tight_layout()
         except:pass
 
