6e6c51b0b2d2e290ff52c6f964c5a332a6809fa1
Thomas Cokelaer <cokelaer@gmail.com>
2016-06-19 00:34:21 +0200

    add missing dependencies for RTD

--- setup.py
+++ setup.py
@@ -54,7 +54,8 @@ if on_rtd is True:  # only import and set the theme if we're building docs
 else:
     install_requires = ['numpy', "numexpr", 'matplotlib>=1.4.3',
         'pandas>=0.16.2', 'easydev>=0.9.18', 'scipy', "colormap>=0.9.7",
-        'jinja2', 'statsmodels', "scikit-learn", "reports>=0.1.9"],
+        'jinja2', 'statsmodels', "scikit-learn", "reports>=0.1.9", 
+        "sphinx-gallery", "numpydoc"],
 
 
 packages = find_packages()
