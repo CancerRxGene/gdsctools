b8fe3bd3edc1fdb299773bdfec9256c3b6d35dc1
elisabeth-chen <ec12@sanger.ac.uk>
2016-06-24 12:05:10 +0100

    Update gdsc1000 related sub-scripts.

---
+++ gdsctools/annotate_genes.py
@@ -0,0 +1,102 @@
+"""
+
+This module concatantes previously fetched GDSC1000 files into a 
+single data frame that can be converted into a BEM and fed into the 
+ANOVA analysis step of gdsctools. 
+If requested, this script has the option to convert CNA and 
+methylation data from their ID values to their gene annotations.
+
+E.D.Chen 2016-06-02
+
+"""
+
+import urllib.request as urlrequest
+import pandas as pd
+
+class MergeGDSCData( object ):
+    """
+
+    This class depends on having previously run the fetch_gdsc_data class in the same folder::
+    
+        an = annotate_genes.MergeGDSCData()
+        an.load_files() # loads previously fetched files into class
+        an.annotate_all() # Optional. adds gene-level annotation to CNA and methylation data
+        an.merge_all() # Concatantes variant_df, cna_df and methyl_df into a single data frame (complete_df)
+        
+    """
+
+    def __init__(self):
+        self.url_base = "http://www.cancerrxgene.org/gdsc1000//Data/suppData/"
+        self.data_folder_name = "./data/gdsc_1000_data/"
+        self.annotate = False
+        
+    def load_files( self, annotate = None ):
+        if annotate == None: 
+            annotate = self.annotate
+        self.variant_df = pd.read_csv( self.data_folder_name + "variant.csv" )
+        self.variant_df['GENE'] = self.variant_df.IDENTIFIER
+        self.cell_line_df = pd.read_csv( self.data_folder_name + "cell_line_dict.csv" )
+        if annotate:
+            self.annotate_all()
+        else:
+            self.cna_df = pd.read_csv( self.data_folder_name + "cna.csv" )
+            self.methyl_df = pd.read_csv( self.data_folder_name + "methylation.csv" )
+        
+    def annotate_all(self):
+        self.annotate = True
+        self._download_annotation()
+        self.methyl_df = self._annotate_methylation()
+        self.cna_df = self._annotate_cna()
+        
+    def merge_all( self ):
+        """ 
+        Concatanates genomic annotations and adds cell line information.
+        If CNA and methylation are annotated, an extra 'GENE' column will be included in self.complete_df
+        """
+        self.variant_df['TYPE'] = 'GENE_VARIANT'
+        self.cna_df['TYPE'] = self.cna_df.ALTERATION_TYPE.str.upper()
+        self.methyl_df['TYPE'] = 'METHYLATION'
+        self.complete_df = pd.concat( [self.variant_df, self.cna_df, self.methyl_df] , axis = 0, join = 'inner' )
+        self.complete_df = self.complete_df.merge( self.cell_line_df, how = 'left', on = ['CELL_LINE', 'TISSUE_FACTOR'])
+
+    def _download_annotation(self):
+        """ 
+        Downloads the annotation files from the cancerrxgene.org/gdsc1000 website.
+        """
+        methylation_annotation_url = self.url_base + "TableS2H.xlsx"
+        cna_annotation_url = self.url_base + "TableS2D.xlsx"
+        urlrequest.urlretrieve( methylation_annotation_url, self.data_folder_name + "methylation_annotation.xlsx")
+        urlrequest.urlretrieve( cna_annotation_url, self.data_folder_name + "cna_annotation.xlsx")
+
+    def _annotate_methylation(self):
+        methyl_df = pd.read_csv(self.data_folder_name + "methylation.csv")
+        xls = pd.ExcelFile(self.data_folder_name + "methylation_annotation.xlsx")
+        methyl_annotation = xls.parse(header = 16)
+        methyl_annotation = methyl_annotation[ ['Cancer Types', 'Genomic Coordinates', 'GN' ] ]
+        methyl_annotation.index = range( len( methyl_annotation.index ) )
+        methyl_annotation.columns = [ 'TISSUE_FACTOR', 'IDENTIFIER', 'GENE' ]
+        methyl_df = methyl_df.merge( methyl_annotation, how = 'left', on = ['IDENTIFIER', 'TISSUE_FACTOR'] )
+        methyl_df = self._string_split( methyl_df, 'GENE', '; ')
+        return methyl_df
+
+    def _annotate_cna(self):
+        cna_df = pd.read_csv(self.data_folder_name + "cna.csv")
+        xls = pd.ExcelFile(self.data_folder_name + "cna_annotation.xlsx")
+        cna_annotation = xls.parse(header = 19)
+        cna_annotation = cna_annotation[ [ 'Identifier', 'Cancer Type', 'Recurrent\nAmplification/\nDeletion', 'nGenes', 'Contained genes' ] ]
+        cna_annotation.columns = ['IDENTIFIER', 'TISSUE_FACTOR', 'CNA_TYPE', 'NGENE', 'GENE']
+        cna_df = cna_df.merge( cna_annotation, how = 'left', on = ['IDENTIFIER', 'TISSUE_FACTOR'] )
+        cna_df = self._string_split( cna_df, 'GENE', ',' )
+        return cna_df
+        
+        
+    def _string_split(self, to_be_split, col_name, separator):
+        """
+        This function splits multiple genes contained within the same genomic region across multiple rows, duplicating all other values.
+        """
+        s = to_be_split[col_name].str.split( separator ).apply(pd.Series, 1).stack()
+        s.index = s.index.droplevel(-1) # Flattens levels to retrieve indices from original frame
+        s.name = col_name # Join requires Series name 
+        del to_be_split[col_name]
+        split_df = to_be_split.join(s)
+        return split_df
---
+++ gdsctools/filter.py
@@ -0,0 +1,137 @@
+"""
+
+To filter complete_df that is outputted from annotate_genes.py
+E.D.Chen 2016-06-05
+
+"""
+
+import pandas as pd
+
+class FilterFeatures( object ):
+    """
+    
+    fl = FilterFeatures( annotated_class ) # This function takes in an object of class annotate_genes that contains at least values for self.annotate and self.complete_df
+    
+    Filters that can then be applied include:
+    
+    filter_by_type( [ 'GENE_VARIANT', 'AMPLIFICATION', 'DELETION', 'METHYLATION' ] )
+    filter_by_tissue_list( ['BRCA', 'SCLSC', ...] ) 
+    filter_by_gene_list( [ 'TP53', 'APC', ... ] ) # Takes list of gene names in HUGO format
+    filter_by_cell_line_list( [ 'U-2-OS', 'AsPC-1', '...' ] ) # Takes list of cell line names. Case sensitive!!
+    filter_by_cosmic_list( [ ... ] ) # Takes list of COSMIC IDs (must be integer or floats)
+    
+    filter_by_recurrence( min_recurrence = 3 ) # Only keeps genomic features that are altered in at least x cell lines. By default, x = min_recurrence = 3
+    
+    
+    At the end, call the function:
+    
+    fl.make_matrix() 
+    # Automatically calls the function filter_by_recurrence with min_recurrence = 3. 
+    # Stores final matrix that can be fed into gdsctools ANOVA part under fl.final_matrix
+    
+    """
+    
+
+
+    def __init__( self, annotated_class ):
+        self.annotate = annotated_class.annotate
+        self.starting_df = annotated_class.complete_df
+        self.starting_df.TISSUE_FACTOR.fillna("UNKNOWN", inplace=True)
+        self.working_df = self.starting_df
+        self.load_filter_dicts() # loads in default dictionaries (decide if we should keep or load optionally)
+        
+    def reset_all_filters( self ):
+        self.working_df = self.starting_df
+
+    def load_filter_dicts( self ):
+        import default_sets
+        self.dicts = default_sets.DefaultDictionaries()
+        
+    def filter_by_type( self, type_list = None ):
+        if type_list == None:
+            print( "Please enter list of types to keep. Acceptable types include: 'GENE_VARIANT', 'AMPLIFICATION', 'DELETION', 'METHYLATION'." )
+        else:
+            type_list = [ x.upper() for x in type_list ]
+            self.working_df = self.working_df.query( "TYPE in @type_list" )
+
+    def filter_by_gene_list( self, gene_list = None ):
+        if gene_list == None:
+            print( "Please enter gene list. To use the GDSC1000 core genes, add argument gene_list = 'Core Genes'." )
+            if self.dicts in globals():
+                print( "Possible dictionary keys include: ", self.dicts.gene_dict.keys() )
+        elif isinstance( gene_list, str ):
+            gene_list = self.dicts.gene_dict[ gene_list ]
+        else:
+            pass
+        gene_list = [ x.upper() for x in gene_list ]
+        self.working_df = self.working_df.query( "GENE in @gene_list" )
+        
+        
+    def filter_by_tissue_list( self, tissue_list = None ):
+        if tissue_list == None:
+            print( "Please enter list of tissues. To use all tissues, add argument tissue_list = 'Complete'" )
+            if self.dicts in globals():
+                print( "Possible dictionary keys include: ", self.dicts.tissue_dict.keys() )
+        elif isinstance( tissue_list, str ):
+            tissue_list = self.dicts.tissue_dict[ tissue_list ]
+        else:
+            pass
+        tissue_list = [ x.upper() for x in tissue_list ]
+        self.working_df = self.working_df.query( "TISSUE_FACTOR in @tissue_list" )
+
+    def filter_by_cosmic_list( self, cosmic_list = None ):
+        if cosmic_list == None:
+            print( "Please enter list of COSMIC IDs. To use an example set of COSMIC IDs, add argument cosmic_list = 'Dummy'" )
+            if self.dicts in globals():
+                print( "Possible dictionary keys include: ", self.dicts.cosmic_dict.keys() )
+        elif isinstance( cosmic_list, str ):
+            cosmic_list = self.dicts.cosmic_dict[ cosmic_list ]
+        else:
+            pass
+        self.working_df = self.working_df.query( "COSMIC_ID in @cosmic_list" )
+        
+        
+    def filter_by_cell_line_list( self, cell_line_list = None ):
+        if cell_line_list == None:
+            print( "Please enter list of cell line names. To use an example set of names, add argument cell_line_list = 'Dummy'" )
+            if self.dicts in globals():
+                print( "Possible dictionary keys include: ", self.dicts.cell_line_dict.keys() )
+        elif isinstance( cell_line_list, str ):
+            cell_line_list = self.dicts.cell_line_dict[ cell_line_list ]
+        else:
+            pass
+        self.working_df = self.working_df.query( "CELL_LINE in @cell_line_list" )
+        
+        
+        
+    def filter_by_recurrence( self, min_recurrence = 3 ):
+        if self.annotate == True: 
+            feature = "GENE"
+        else:
+            feature = "IDENTIFIER"
+            
+        flatten_cell_lines = self.working_df.groupby( [ feature, "COSMIC_ID", "TISSUE_FACTOR" ], as_index = False )[[ "TYPE" ]].count() 
+        feature_count = flatten_cell_lines.groupby( [feature], as_index = False )[[ "COSMIC_ID" ]].count() 
+        self.feature_count = feature_count[ feature_count["COSMIC_ID"] >= min_recurrence ]
+
+        self.working_df = self.working_df[ self.working_df[ feature ].isin( self.feature_count[ feature ] ) ]
+
+
+
+    # Make into matrix
+    def make_matrix( self, min_recurrence = 3 ):
+        """
+        Return a dataframe compatible with ANOVA analysis
+        """
+        
+        self.filter_by_recurrence( min_recurrence = 3 )
+        
+        if self.annotate == True: 
+            feature = "GENE"
+        else:
+            feature = "IDENTIFIER"    
+        
+        final_matrix = pd.crosstab( self.working_df[ feature ], columns=[ self.working_df["COSMIC_ID"], self.working_df['TISSUE_FACTOR'], self.working_df["CELL_LINE"], self.working_df[ "MSI_FACTOR" ], self.working_df[ "MEDIA_FACTOR"] ] )
+        final_matrix[ final_matrix > 1 ] = 1
+        self.final_matrix = final_matrix.T.reset_index()
+        self.final_matrix.to_csv( "./data/gdsc_1000_data/features_matrix.csv", index = False )
