6b3ac89f50c7884feef69a984139e39ee5bd182b
Thomas Cokelaer <cokelaer@gmail.com>
2017-03-09 11:13:13 +0100

    clean tests

--- test/gdsctools/test_pipelines.py
+++ test/gdsctools/test_pipelines.py
@@ -1,64 +1,46 @@
 from gdsctools.scripts import anova as pipelines
 from gdsctools import  ic50_test
 
-from nose.plugins.attrib import attr
 
-
+prog = "gdsctools"
+filename = ic50_test.filename
 
 class TestPipeline(object):
 
-    @classmethod
-    def setup_class(klass):
-        """This method is run once for each class before any tests are run"""
-        klass.prog = "gdsctools"
-        klass.filename = ic50_test.filename
-        klass.params = {'prog': klass.prog, 'filename': klass.filename}
-
-    @classmethod
-    def teardown_class(klass):
-        """This method is run once for each class _after_ all tests are run"""
-
-    def setUp(self):
-        """This method is run once before _each_ test method is executed"""
-
-    def teardown(self):
-        """This method is run once after _each_ test method is executed"""
-
     def test_version(self):
-        pipelines.main([self.prog, '--version'])
+        pipelines.main([prog, '--version'])
 
     def test_help(self):
-        pipelines.main([self.prog, '--help'])
-        pipelines.main([self.prog])
+        pipelines.main([prog, '--help'])
+        pipelines.main([prog])
 
     def test_print_drug_names(self):
-        pipelines.main([self.prog, '--input-ic50', self.filename,
+        pipelines.main([prog, '--input-ic50', filename,
             '--print-drug-names'])
     def test_print_tissue_names(self):
-        pipelines.main([self.prog, '--input-ic50', self.filename,
+        pipelines.main([prog, '--input-ic50', filename,
             '--print-tissue-names'])
     def test_print_feature_names(self):
-        pipelines.main([self.prog, '--input-ic50', self.filename,
+        pipelines.main([prog, '--input-ic50', filename,
             '--print-feature-names'])
 
     def test_odaf(self):
-        pipelines.main([self.prog, '--input-ic50', self.filename,
+        pipelines.main([prog, '--input-ic50', filename,
             '--drug', '1047', '--no-html'])
 
     def test_odof(self):
-        pipelines.main([self.prog, '--input-ic50', self.filename,
+        pipelines.main([prog, '--input-ic50', filename,
             '--drug', '1047', '--no-html', '--feature', 'TP53_mut'])
-        pipelines.main([self.prog, '--input-ic50', self.filename,
+        pipelines.main([prog, '--input-ic50', filename,
             '--drug', '1047', '--feature', 'TP53_mut'])
 
-
     # slow takes about 30-60 seconds
-    @attr('skip')
     def test_adaf(self):
-        pipelines.main([self.prog, '--input-ic50', self.filename,
+        pipelines.main([prog, '--input-ic50', filename,
             '--no-html'])
 
     def test_summary(self):
-        pipelines.main([self.prog, '--summary'])
+        pipelines.main([prog, '--summary'])
+
     def test_summary(self):
-        pipelines.main([self.prog, '--testing'])
+        pipelines.main([prog, '--testing'])
