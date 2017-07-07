0c7cf5c32944a5a4c3d750c7e2e54ffd2c84eee6
Thomas Cokelaer <cokelaer@gmail.com>
2017-07-07 21:24:58 +0200

    fix dependencies in setup

--- setup.py
+++ setup.py
@@ -56,8 +56,8 @@ if on_rtd is True:  # only import and set the theme if we're building docs
                         "colorlog"]
 else:
     install_requires = ['numpy', "numexpr", 'matplotlib>=1.4.3',
-        'pandas==0.20.1', 'easydev>=0.9.34', 'scipy==0.19.0', "colormap==1.0.1",
-        'jinja2', 'statsmodels==0.8.0', "scikit-learn==0.18.1", "reports==0.3.1",
+        'pandas==0.20.1', 'easydev>=0.9.34', 'scipy==0.19.1', "colormap==1.0.1",
+        'jinja2', 'statsmodels==0.8.0', "scikit-learn==0.18.2", "reports==0.3.1",
         "biokit==0.4.1", "colorlog", "xlrd"],
 
 
